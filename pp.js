a9118eec378b682d6050e6c42ad2b4e569e22034
JustYourAverageJosh <47060866+JustYourAverageJosh@users.noreply.github.com>
2024-09-19 00:20:03 -0400

    setup postgresDB, paypal, added Header. In dev at least

---
+++ pp.js
@@ -0,0 +1,113 @@
+window.paypal
+  .Buttons({
+    async createOrder() {
+      const container = document.querySelector("#result-message");
+      container.innerHTML = '';
+      try {
+        const response = await fetch("/api/orders", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // use the "body" param to optionally pass additional order information
+          // like product ids and quantities
+          body: JSON.stringify({
+            product:
+              {
+                description: "Mekoski Wine Tasking Event Ticket",
+                quantity: 1
+              },
+          }),
+        });
+        
+        const orderData = await response.json();
+        
+        if (orderData.id) {
+          return orderData.id;
+        } else {
+          // const errorDetail = orderData?.details?.[0];
+          // const errorMessage = errorDetail
+          //   ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
+          //   : JSON.stringify(orderData);
+          const errorMessage = orderData.error || "Unknown error occurred";
+          
+          throw new Error(errorMessage);
+        }
+      } catch (error) {
+        console.error(error);
+        resultMessage(`Could not initiate PayPal Checkout...<br><br>${error}`);
+      }
+    },
+    async onApprove(data, actions) {
+      try {
+        const response = await fetch(`/api/orders/${data.orderID}/capture`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        
+        const orderData = await response.json();
+        // Three cases to handle:
+        //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+        //   (2) Other non-recoverable errors -> Show a failure message
+        //   (3) Successful transaction -> Show confirmation or thank you message
+        
+        const errorDetail = orderData?.details?.[0];
+        
+        if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+          // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+          // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+          errorDetail.description+="<br><br>There was an issue processing your payment. Please try again or use a different payment source."
+          throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
+          return actions.restart();
+        } else if (errorDetail) {
+          // (2) Other non-recoverable errors -> Show a failure message
+          throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
+        } else if (!orderData.purchase_units) {
+          throw new Error(JSON.stringify(orderData));
+        } else {
+          // (3) Successful transaction -> Show confirmation or thank you message
+          // Or go to another URL:  actions.redirect('thank_you.html');
+          const transaction =
+            orderData?.purchase_units?.[0]?.payments?.captures?.[0] ||
+            orderData?.purchase_units?.[0]?.payments?.authorizations?.[0];
+          resultMessage(orderData,true,orderData.credit)
+          // console.log(
+          //   "Capture PP result",
+          //   orderData,
+          //   JSON.stringify(orderData, null, 2),
+          // );
+        }
+      } catch (error) {
+        console.error(error);
+        resultMessage(
+          `Sorry, your transaction could not be processed...<br><br>${error}`,
+        );
+      }
+    },
+  })
+  .render("#paypal-button-container");
+  
+// Example function to show a result to the user. Your site's UI library can be used instead.
+function resultMessage(message,success=false,db=true) {
+  if (success){
+    const pricing=document.getElementById('pricing'); pricing.style.lineHeight='30px'
+    if (!db){
+      pricing.innerHTML=`We have received your payment.<br>This purchase is currently<span style='color:red;font-size:larger;'> Pending</span>, 
+      and will be processed within the next few hours. <br>You will receive an email when it has been completed!<br>
+      Purchase #: ${message.purchaseID}<br>Credits: ${message.quantity}<br> Cost: $${message.cost}`
+    } else {
+      pricing.innerHTML=`Purchase ID: ${message.purchaseID}<br>Credits: ${message.quantity}<br> Cost: $${message.cost}
+      <br> Thank You for your purchase!<br>You now have ${message.credit} credits!<br> The purchase details have also been sent to your email.
+      <br>Please go to <a href='/dashboard'>Your Dashboard</a> to create an order!`}
+      document.getElementById('nav-credits').textContent=message.credit.toString()
+  }
+  else{
+    const container = document.querySelector("#result-message");
+    const parent = container.parentNode;
+    parent.insertBefore(container, parent.firstChild);
+    container.style.color = "red";
+    container.innerHTML = message;
+  }
+}
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const { pool } = require("./dbConfig");
+const {escapeLiteral} =require('pg');
+const MailChecker=require('mailchecker')
+const nodemailer = require("nodemailer");
 
 
 const isProduction = process.env.NODE_ENV === "production";
@@ -23,13 +27,31 @@ app.use(express.static(__dirname));/////////PUT ALL JS OTHER THAN SERVER.JS INTO
 
 const PORT = process.env.PORT || 4000;
 
+
+
+///ROUTES
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index',{prod:isProduction})
 })
 
+app.get('/tickets', async (req,res) =>{
+    try {
+        const result = await pool.query(
+            `SELECT count(*) as row_count FROM tickets`
+        )
+        const ticket_count=result.rows[0].row_count
+        return res.send(ticket_count)
+    } catch (error) {
+        console.error('Error getting ticket count:', error);
+        return res.status(500).send('An internal server error occurred.');
+    }
+})
+////ROUTES
 
 
 
+
+////END
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}` )
 });
